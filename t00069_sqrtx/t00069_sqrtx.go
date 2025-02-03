ffbdafb045cf9a19d8fe2298e0a376ace7057267
Yarik Kotsur <yarikkotsur@gmail.com>
2025-02-03 12:58:44 +0100

    69. sqrtx fixes

--- t00069_sqrtx/t00069_sqrtx.go
+++ t00069_sqrtx/t00069_sqrtx.go
@@ -1,58 +1,22 @@
 package t00069_sqrtx
 
-const MAX_X = (1 << 32) - 1
-const APPROX_NR_OF_ELEMENTS = 1 << 16
-
-var solutionsTable []int
-
-func getSolutionsTable() []int {
-	if solutionsTable == nil {
-		solutionsTable = make([]int, 0, APPROX_NR_OF_ELEMENTS)
-
-		for i := 0; i*i < MAX_X; i++ {
-			solutionsTable = append(solutionsTable, i*i)
-		}
-	}
-
-	return solutionsTable
-}
+const Min = 0
+const Max = int(float64(1<<15) * 1.5)
 
 func mySqrt(x int) int {
-	srcTable := getSolutionsTable()
+	start, end := Min, Max
 
-	return binSearch(
-		srcTable,
-		x,
-		0,
-		len(srcTable)-1,
-	)
-}
+	for {
+		if end-start == 1 {
+			return start
+		}
 
-func binSearch(
-	srcTable []int,
-	x int,
-	start int,
-	end int,
-) int {
-	if end-start == 1 {
-		return start
-	}
+		middle := start + (end-start)/2
 
-	middle := start + (end-start)/2
-
-	if x < srcTable[middle] {
-		return binSearch(
-			srcTable,
-			x,
-			start,
-			middle,
-		)
-	} else {
-		return binSearch(
-			srcTable,
-			x,
-			middle,
-			end,
-		)
+		if x < middle*middle {
+			end = middle
+		} else {
+			start = middle
+		}
 	}
 }
