ee25d32528c0290507959af2d8860becb3a28e4c
Yarik Kotsur <yarikkotsur@gmail.com>
2025-02-04 14:03:34 +0100

    t 00094

--- t00094_binary_tree_inorder_traversal/t00094_binary_tree_inorder_traversal.go
+++ t00094_binary_tree_inorder_traversal/t00094_binary_tree_inorder_traversal.go
@@ -1,1 +1,27 @@
 package t00094_binary_tree_inorder_traversal
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	res := make([]int, 0)
+
+	traverse(root, func(i int) {
+		res = append(res, i)
+	})
+
+	return res
+}
+
+func traverse(root *TreeNode, fn func(int)) {
+	if root == nil {
+		return
+	}
+
+	traverse(root.Left, fn)
+	fn(root.Val)
+	traverse(root.Right, fn)
+}
